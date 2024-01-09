8aaeaa828605c5e56a8f4358c0bed2d85e71639c
xup60521 <84887662+xup60521@users.noreply.github.com>
2024-01-09 23:04:56 +0800

    feature: check stops passby

--- src/app/_trpc/serverClient.ts
+++ src/app/_trpc/serverClient.ts
@@ -5,7 +5,7 @@ import { createTRPCProxyClient, httpBatchLink } from "@trpc/client";
 export const serverClient = createTRPCProxyClient<AppRouter>({
     links: [
         httpBatchLink({
-            url: "http://localhost:3000/api/trpc"
+            url: "/api/trpc"
         })
     ]
 })
--- src/app/bus/page.tsx
+++ src/app/bus/page.tsx
@@ -5,12 +5,14 @@ import type { BusRouteType } from "@/type/BusRouteType"
 import { trpc } from "../_trpc/client"
 import Select from "react-select"
 import { useEffect, useId, useState } from "react"
+import { FaArrowRightLong } from "react-icons/fa6";
 
 export default function Bus() {
 
    const data = trpc.getAllBus.useQuery() as AllBusType
    const [bus, setBus] = useState("")
    const routeDetail = trpc.getBusRoute.useQuery(bus ?? "") as BusRouteType
+   const [order, setOrder] = useState(0)
    const selectOptions = data.data?.map(d=>{
     return {
         "value": d.RouteName.Zh_tw,
@@ -18,20 +20,23 @@ export default function Bus() {
         "key": d.RouteName.Zh_tw,
     }
    })
-   const selectKeys = data.data?.map(d=>{
-    return d.RouteName.Zh_tw
-   })
+   const isOneWay = (routeDetail.data.filter((item)=>item.RouteName.Zh_tw === bus).length === 1) ? true : false
 
     return (
         <div className=" bg-slate-900 w-screen h-screen text-white overflow-x-hidden flex flex-col justify-center items-center">
             <div className="flex h-5/6">
-                <div className="w-96 h-full p-4 flex flex-col items-center gap-2">
+                <div className="w-96 h-full p-4 flex flex-col items-center gap-4">
                     <p className="font-black">---選擇路線---</p>
                     <Select onChange={(e)=>setBus(e?.value ?? "")} options={selectOptions} instanceId={useId()} className="text-black w-full" />
-                    <div className=" flex-grow w-full overflow-y-auto flex flex-col gap-2">
-                        <StopList routeDetail={routeDetail} direction={1} bus={bus} />
+                    <div>
+                        {!isOneWay && <button onClick={()=>{
+                            setOrder((prev)=>{
+                                if (prev === 1) {return 0}
+                                return 1
+                            })
+                        }}>Change Direction</button>}
                     </div>
-                    
+                    <StopList routeDetail={routeDetail} direction={order} bus={bus} />                    
                 </div>
             </div>
         </div>
@@ -53,13 +58,20 @@ function StopList({routeDetail, direction, bus}: {routeDetail: BusRouteType, dir
 
     return (
         <>
-            {filteredData.map(d=>{
-                return (
-                    <div key={d.StopName.Zh_tw}>
-                        <span className="text-xl">{d.StopName.Zh_tw}</span>
-                    </div>
-                )
-            })}
+            <div className="flex items-center justify-center gap-2 bg-slate-200 text-slate-900 font-bold border-2 rounded-md p-2 w-full">
+                <span className="w-2/5 text-center">{filteredData[0].StopName.Zh_tw}</span>
+                <span className="w-1/5 flex justify-center "><FaArrowRightLong /></span>
+                <span className="w-2/5 text-center">{filteredData.reverse()[0].StopName.Zh_tw}</span>
+            </div>
+            <div className=" flex-grow w-full overflow-y-auto flex flex-col gap-2">
+                {filteredData.reverse().map(d=>{
+                    return (
+                        <div key={d.StopName.Zh_tw}>
+                            <span className="text-xl">{d.StopName.Zh_tw}</span>
+                        </div>
+                    )
+                })}
+            </div>
         </>
     )
 }

b3a42798ffa6c2341cd52e214de4056a6f0c4bc2
xup60521 <84887662+xup60521@users.noreply.github.com>
2024-01-10 19:47:21 +0800

    fix: select list order

--- src/app/bus/Bus.tsx
+++ src/app/bus/Bus.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { publicENV } from "@/lib/publicENV"
-import type { Datum }  from "@/type/AllBusType" 
+import type { AllBusType }  from "@/type/AllBusType" 
 import type { BusRouteType } from "@/type/BusRouteType"
 import { trpc } from "../_trpc/client"
 import { useEffect, useId, useState } from "react"
@@ -11,19 +11,21 @@ import StopList from "./_component/StopList"
 import StopsMarker from "./_component/StopsMaker"
 import { useRouter } from "next/navigation"
 
-export default function Bus({initData}: {initData: Datum[]}) {
+export default function Bus({initData}: {initData: AllBusType["data"]}) {
     
     const router = useRouter()
     const [bus, setBus] = useState("")
     const routeDetail = trpc.getBusRoute.useQuery(bus ?? "") as BusRouteType
     const [order, setOrder] = useState(0)
     const [position, setPosition] = useState({ lat: 24.137396608878987, lng: 120.68692065044608 });
+    const regex1 = /^[\u4e00-\u9fa5]/
+    const regex2 = /[\u4e00-\u9fa5][0-9]/
+    const regex3 = /[\u4e00-\u9fa5A-Z()]/g
     const selectOptions = initData.map(d=>{
     return {
         "value": d.RouteName.Zh_tw,
         "label": `${d.RouteName.Zh_tw} ${d.SubRoutes[0].Headsign}`,
-    }
-    })
+    }}).sort((a,b)=>Number(a.value.replace(regex1, "").replace(regex2, "").replace(regex3, "")) - Number(b.value.replace(regex1, "").replace(regex2, "").replace(regex3, "")))
     let isOneWay = false;
     if (Array.isArray(routeDetail.data)) {
         isOneWay = (routeDetail.data.filter((item)=>item.RouteName.Zh_tw === bus).length === 1) ? true : false
@@ -52,14 +54,20 @@ export default function Bus({initData}: {initData: Datum[]}) {
                 <div className="flex h-full w-full">
                     <div className="bg-white bg-opacity-50 backdrop-blur-lg  w-96 h-full p-4 flex flex-col items-center gap-4 z-50">
                         <p className="font-black text-lg">---選擇路線---</p>
-                        <Select onChange={(e)=>setBus(e?.value ?? "")} options={selectOptions} instanceId={useId()} className="text-black w-full" />
-                        <div>
-                            {!isOneWay && <button onClick={()=>{
-                                if (order === 1) {setOrder(0)} 
-                                else {setOrder(1)}
-                            }}>Change Direction</button>}
-                        </div>
-                        <StopList routeDetail={routeDetail} direction={order} bus={bus} />                    
+                        <Select onChange={(e)=>{
+                            setBus(e?.value ?? "")
+                            console.log(e?.value);
+                            
+                        }} options={selectOptions} instanceId={useId()} className="text-black w-full" />
+                        {Boolean(bus) && <>
+                            <div>
+                                {!isOneWay && <button onClick={()=>{
+                                    if (order === 1) {setOrder(0)} 
+                                    else {setOrder(1)}
+                                }}>Change Direction</button>}
+                            </div>
+                            <StopList routeDetail={routeDetail} direction={order} bus={bus} />     
+                        </>}                         
                     </div>
                 </div>
                 <div className="absolute h-screen w-screen z-10">
--- src/app/bus/page.tsx
+++ src/app/bus/page.tsx
@@ -1,10 +1,11 @@
+import { AllBusType } from "@/type/AllBusType"
 import { serverClient } from "../_trpc/serverClient"
 import Bus from "./Bus"
 
 export default async function Page() {
 
-    const data = await serverClient.getAllBus.query() 
-
+    const data = await serverClient.getAllBus.query() as AllBusType["data"]
+    
     return (
         <>
             <Bus initData={data} />
